4f90fb31f777043cf919ef5d21bc28fe93c4468b
Sivcheu <choeng.sivcheu02@gmail.com>
2023-02-01 15:25:14 +0700

    profile module

--- app/Modules/Profile/Profile.php
+++ app/Modules/Profile/Profile.php
@@ -2,6 +2,9 @@
 
 namespace App\Modules\Profile;
 
+use App\Modules\JobDetail\JobDetail;
+use App\Modules\User\User;
+use App\Modules\Workspace\Workspace;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Passport\HasApiTokens;
@@ -11,4 +14,40 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Profile extends Model
 {
     use HasFactory, HasApiTokens, Notifiable, SoftDeletes;
+
+    protected $table = 'profiles';
+
+    protected $fillable = [
+        'name',
+        'alias',
+        'avatar',
+        'phone',
+        'email',
+        'job_detail_id',
+        'user_id',
+        'workspace_id',
+    ];
+
+    protected $hidden = [
+        'deleted_at',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function jobDetail()
+    {
+        return $this->belongsTo(JobDetail::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function workspace()
+    {
+        return $this->belongsTo(Workspace::class);
+    }
 }
