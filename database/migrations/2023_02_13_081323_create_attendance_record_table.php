e234a435f77154901f2d6ad04f1dbdfe0977dcce
Sivcheu <choeng.sivcheu02@gmail.com>
2023-02-13 15:39:59 +0700

    migration

---
+++ database/migrations/2023_02_13_081323_create_attendance_record_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance_record', function (Blueprint $table) {
+            $table->id();
+            $table->string('clock_in');
+            $table->string('clock_out');
+            $table->date('date');
+            $table->unsignedBigInteger('profile_id');
+            $table->unsignedBigInteger('workspace_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance_record');
+    }
+};
